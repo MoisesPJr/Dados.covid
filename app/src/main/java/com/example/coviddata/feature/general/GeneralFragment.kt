7229c099eca7ff9fbfeee8f409499d772d0309a5
moisespereirajunior-carguero <moises.junior@carguero.com.br>
2021-11-08 22:19:27 -0300

    feat(config_project) Configurado drawer e endpoints

--- app/src/main/java/com/example/coviddata/feature/general/GeneralFragment.kt
+++ app/src/main/java/com/example/coviddata/feature/general/GeneralFragment.kt
@@ -1,50 +1,33 @@
 package com.example.coviddata.feature.general
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.example.coviddata.R
-import com.example.coviddata.feature.country.domain.Data
-import com.example.coviddata.feature.country.domain.Teste
-import com.example.coviddata.plugin.CountryViewModel
+import com.example.coviddata.databinding.FragmentGeneralBinding
 
 
 class GeneralFragment : Fragment() {
 
 
-    private lateinit var viewModel: CountryViewModel
-
+    private var _binding: FragmentGeneralBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_general, container, false)
+        _binding = FragmentGeneralBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-     viewModel = ViewModelProviders.of(this).get(CountryViewModel(requireActivity().application)::class.java)
-        registrationObserver()
-
-        viewModel.getCountries("brazil")
 
     }
 
-    private fun registrationObserver(){
-        viewModel.data.observe(viewLifecycleOwner,countryObserver )
-    }
 
-    private val countryObserver = Observer<Teste>{ country ->
 
-    if(country != null){
-    }
-
-    }
 }
